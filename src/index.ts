6d70293b97583ec38935548bac27fe9f8c2afcdf
monokh <mnokhb@gmail.com>
2022-03-28 13:23:52 +0100

    Make setup wallet sync

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,10 @@ import store from './store';
 import { WalletOptions } from './types';
 import { walletOptionsStore } from './walletOptions';
 
-async function setupWallet(options: WalletOptions) {
+function setupWallet(options: WalletOptions) {
   walletOptionsStore.setOptions(options);
   if (options.initialState)
-    await store.commit.SET_STATE({ newState: options.initialState });
+    store.commit.SET_STATE({ newState: options.initialState });
 
   return store;
 }
