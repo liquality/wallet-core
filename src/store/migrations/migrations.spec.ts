f04443c2852952b53aad799ea2a9f8b3bec84263
monokh <mnokhb@gmail.com>
2022-04-01 16:19:18 +0100

    move test

---
+++ src/store/migrations/migrations.spec.ts
@@ -0,0 +1,18 @@
+import { processMigrations } from './'
+import buildConfig from '../../build.config'
+
+test('Migrates to v2', async () => {
+  expect(await processMigrations({ network: 'mainnet', activeWalletId: '123-123-123' })).toEqual({
+    version: 2,
+    network: 'mainnet',
+    activeWalletId: '123-123-123',
+    assets: {
+      mainnet: {
+        '123-123-123': buildConfig.defaultAssets.mainnet
+      },
+      testnet: {
+        '123-123-123': buildConfig.defaultAssets.testnet
+      }
+    }
+  })
+})
