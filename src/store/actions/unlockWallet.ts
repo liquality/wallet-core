af4455fc4346ac9bbd53e086e4483af2c32e48fa
monokh <mnokhb@gmail.com>
2022-04-03 23:50:00 +0100

    Call wallet options for crypto. remove legacy crypto

--- src/store/actions/unlockWallet.ts
+++ src/store/actions/unlockWallet.ts
@@ -1,31 +1,18 @@
-import { encrypt, decrypt, decryptLegacy } from '../../utils/crypto'
+import { decrypt } from '../../utils/crypto';
 
 export const unlockWallet = async ({ commit, state }, { key }) => {
-  let wallets = await decrypt(state.encryptedWallets, key, state.keySalt)
-
-  // Migration to new encryption method
-  // TODO: to be removed
-  if (!wallets) {
-    wallets = await decryptLegacy(state.encryptedWallets, key)
-    if (wallets) {
-      const { encrypted: encryptedWallets, keySalt } = await encrypt(wallets, key)
-      commit('CREATE_WALLET', {
-        keySalt,
-        encryptedWallets,
-        wallet: JSON.parse(wallets)[0]
-      })
-    }
-  }
-  // Migration to new encryption method
+  let wallets = await decrypt(state.encryptedWallets, key, state.keySalt);
 
   if (!wallets) {
-    throw new Error('Try Again. Enter the right password (it has 8 or more characters).')
+    throw new Error(
+      'Try Again. Enter the right password (it has 8 or more characters).'
+    );
   }
 
-  const parsedWallets = JSON.parse(wallets)
+  const parsedWallets = JSON.parse(wallets);
   commit('UNLOCK_WALLET', {
     key,
     wallets: parsedWallets,
-    unlockedAt: Date.now()
-  })
-}
+    unlockedAt: Date.now(),
+  });
+};
