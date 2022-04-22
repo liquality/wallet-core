7707624a6b2b7c2541eb3ccdbac41eebfc1d83ce
Raj Beemi <beemi.raja@gmail.com>
2022-04-22 15:58:51 +0100

    test: custom token tests

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,6 +1,8 @@
 import {setupWallet} from './index';
 import defaultWalletOptions from './walletOptions/defaultOptions';
 import buildConfig from "./build.config";
+import {Network} from "./store/types";
+import {ChainId} from "@liquality/cryptoassets";
 
 test('Initial State with RSK Legacy Derivation', async () => {
     const wallet = await setupWallet(defaultWalletOptions);
@@ -91,7 +93,7 @@ test('Should be able to validate assets with analytics false', async () => {
     expect(testnetEnabledAssets?.length).toBeGreaterThan(1);
 })
 
-test('Should be able to validate analytics true', async () => {
+test('Should be able to create wallet with validate analytics true', async () => {
     const wallet = await setupWallet(defaultWalletOptions);
     await wallet.dispatch.createWallet({
         key: '0x1234567890123456789012345678901234567890',
@@ -114,6 +116,64 @@ test('Should be able to validate analytics true', async () => {
     expect(wallet.state.analytics.askedDate).not.toBe(0);
     expect(wallet.state.analytics.askedTimes).toBe(0);
     expect(wallet.state.analytics.notAskAgain).toBe(false);
+})
+
+test('Should be able create wallet and add custom tokens', async () => {
+    const wallet = await setupWallet(defaultWalletOptions);
+    await wallet.dispatch.createWallet({
+        key: '0x1234567890123456789012345678901234567890',
+        mnemonic: 'test',
+        imported: true,
+    });
+    await wallet.dispatch.unlockWallet({
+        key: '0x1234567890123456789012345678901234567890',
+    });
+    expect(wallet.state.wallets.length).toBe(1);
+    expect(wallet.state.wallets[0].imported).toBe(true);
+    expect(wallet.state.unlockedAt).not.toBe(0);
+
+    await wallet.dispatch.initializeAnalyticsPreferences({
+        accepted: true,
+    });
+
+    expect(wallet.state.analytics.userId).not.toBe(null);
+    expect(wallet.state.analytics.acceptedDate).not.toBe(0);
+    expect(wallet.state.analytics.askedDate).not.toBe(0);
+    expect(wallet.state.analytics.askedTimes).toBe(0);
+    expect(wallet.state.analytics.notAskAgain).toBe(false);
+
+    await wallet.dispatch.addCustomToken({
+        network: Network.Mainnet,
+        walletId: wallet.state.activeWalletId,
+        chain: ChainId.Ethereum,
+        symbol: 'TST',
+        name: 'Test Token',
+        contractAddress: '0x1234567890123456789012345678901234567890',
+        decimals: 18
+    });
+
+    expect(wallet.state.customTokens.mainnet?.[wallet.state.activeWalletId][0].symbol).toBe("TST");
+    expect(wallet.state.customTokens.mainnet?.[wallet.state.activeWalletId][0].name).toBe("Test Token");
+    expect(wallet.state.customTokens.mainnet?.[wallet.state.activeWalletId][0].contractAddress).toBe("0x1234567890123456789012345678901234567890");
+    expect(wallet.state.customTokens.mainnet?.[wallet.state.activeWalletId][0].decimals).toBe(18);
+    expect(wallet.state.customTokens.mainnet?.[wallet.state.activeWalletId][0].chain).toBe("ethereum");
+
+    await wallet.dispatch.addCustomToken({
+        network: Network.Testnet,
+        walletId: wallet.state.activeWalletId,
+        chain: ChainId.Bitcoin,
+        symbol: 'BCTS',
+        name: 'Test Bitcoin',
+        contractAddress: '0x1234567890123456789012343333378901234567890',
+        decimals: 11
+    });
+    console.log(JSON.stringify(wallet.state));
+    expect(wallet.state.customTokens.testnet?.[wallet.state.activeWalletId][0].symbol).toBe("BCTS");
+    expect(wallet.state.customTokens.testnet?.[wallet.state.activeWalletId][0].name).toBe("Test Bitcoin");
+    expect(wallet.state.customTokens.testnet?.[wallet.state.activeWalletId][0].contractAddress).toBe("0x1234567890123456789012343333378901234567890");
+    expect(wallet.state.customTokens.testnet?.[wallet.state.activeWalletId][0].decimals).toBe(11);
+    expect(wallet.state.customTokens.testnet?.[wallet.state.activeWalletId][0].chain).toBe("bitcoin");
+
 })
 
 
