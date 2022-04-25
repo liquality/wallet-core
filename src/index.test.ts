f395ca9e04e61608dd7571d3bbdbb42a17efff01
Raj Beemi <beemi.raja@gmail.com>
2022-04-25 17:08:06 +0100

    test: export private key tests seperated

--- src/index.test.ts
+++ src/index.test.ts
@@ -292,38 +292,4 @@ test('Should be able create wallet and add custom token and remove token', async
   expect(wallet.state.customTokens.mainnet?.[wallet.state.activeWalletId].length).toBe(0);
 });
 
-test('should be ble to export private key', async () => {
-  const wallet = await setupWallet(defaultWalletOptions);
-  await wallet.dispatch.createWallet({
-    key: '0x1234567890123456789012345678901234567890',
-    mnemonic: 'test',
-    imported: true,
-  });
-  await wallet.dispatch.unlockWallet({
-    key: '0x1234567890123456789012345678901234567890',
-  });
-  expect(wallet.state.wallets.length).toBe(1);
-  expect(wallet.state.wallets[0].imported).toBe(true);
-  expect(wallet.state.unlockedAt).not.toBe(0);
-
-  await wallet.dispatch.initializeAnalyticsPreferences({
-    accepted: true,
-  });
-
-  expect(wallet.state.analytics.userId).not.toBe(null);
-  expect(wallet.state.analytics.acceptedDate).not.toBe(0);
-  expect(wallet.state.analytics.askedDate).not.toBe(0);
-  expect(wallet.state.analytics.askedTimes).toBe(0);
-  expect(wallet.state.analytics.notAskAgain).toBe(false);
-
-  const privateKey = await wallet.dispatch.exportPrivateKey({
-    network: Network.Mainnet,
-    walletId: wallet.state.activeWalletId,
-    accountId: '0',
-    chainId: ChainId.Ethereum,
-  });
-  expect(privateKey).not.toBeNull();
-  expect(privateKey).not.toBe(undefined);
-});
-
 
