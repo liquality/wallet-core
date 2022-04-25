38daea9d7cc5e15f4c9007fc5316ce833b35e00d
Raj Beemi <beemi.raja@gmail.com>
2022-04-25 17:06:29 +0100

    test: Remove setup wallet from tests

--- src/store/actions/accounts/toggleAccount.test.ts
+++ src/store/actions/accounts/toggleAccount.test.ts
@@ -7,9 +7,6 @@ let wallet: any;
 beforeEach(async () => {
   jest.useFakeTimers();
   wallet = await setupWallet(defaultWalletOptions);
-  await wallet.dispatch.setupWallet({
-    key: '0x1234567890123456789012345678901234567890',
-  });
   await wallet.dispatch.createWallet({
     key: '0x1234567890123456789012345678901234567890',
     mnemonic: 'rough symbol license spirit advance pact catalog vibrant dream great usage empty',
