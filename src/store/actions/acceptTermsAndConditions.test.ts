bc277e4c4e35a6ac376a1f2c9d781df4ee571b93
Raj Beemi <beemi.raja@gmail.com>
2022-04-30 17:13:54 +0100

    test: acceptTermsAndConditions fixed

--- src/store/actions/acceptTermsAndConditions.test.ts
+++ src/store/actions/acceptTermsAndConditions.test.ts
@@ -4,12 +4,14 @@ import defaultWalletOptions from '../../walletOptions/defaultOptions';
 describe('acceptTermsAndConditions', () => {
   it('should be able to acceptTermsAndConditions', async () => {
     const wallet = await setupWallet(defaultWalletOptions);
-    await wallet.dispatch.acceptTermsAndConditions({ analyticsAccepted: true });
     await wallet.dispatch.createWallet({
       key: '0x1234567890123456789012345678901234567890',
       mnemonic: 'test',
       imported: true,
     });
+
+    await wallet.dispatch.acceptTermsAndConditions({ analyticsAccepted: true });
+
     await wallet.dispatch.unlockWallet({
       key: '0x1234567890123456789012345678901234567890',
     });
