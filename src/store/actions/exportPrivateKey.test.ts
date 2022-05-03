1c3ca0785512aa2e205a6363bfa68d91f9e125d8
Raj Beemi <beemi.raja@gmail.com>
2022-05-03 19:07:14 +0100

    test: fix export private key game

--- src/store/actions/exportPrivateKey.test.ts
+++ src/store/actions/exportPrivateKey.test.ts
@@ -13,26 +13,22 @@ test('should be able to export private key', async () => {
   await wallet.dispatch.unlockWallet({
     key: '0x1234567890123456789012345678901234567890',
   });
-  expect(wallet.state.wallets.length).toBe(1);
-  expect(wallet.state.wallets[0].imported).toBe(true);
-  expect(wallet.state.unlockedAt).not.toBe(0);
+  const walletId = wallet.state.activeWalletId;
+  expect(walletId).not.toBeNull();
+  const mainnetAccounts = wallet?.state?.enabledAssets?.mainnet?.[walletId];
+  expect(mainnetAccounts).not.toBeNull();
 
-  await wallet.dispatch.initializeAnalyticsPreferences({
-    accepted: true,
-  });
-
-  expect(wallet.state.analytics.userId).not.toBe(null);
-  expect(wallet.state.analytics.acceptedDate).not.toBe(0);
-  expect(wallet.state.analytics.askedDate).not.toBe(0);
-  expect(wallet.state.analytics.askedTimes).toBe(0);
-  expect(wallet.state.analytics.notAskAgain).toBe(false);
+  const account = wallet.state.accounts?.[walletId]?.mainnet?.[1];
+  expect(account?.chain).toBe(ChainId.Ethereum);
+  const ethAccountId = account?.id;
 
   const privateKey = await wallet.dispatch.exportPrivateKey({
     network: Network.Mainnet,
-    walletId: wallet.state.activeWalletId,
-    accountId: '0',
+    walletId: walletId,
+    accountId: ethAccountId!,
     chainId: ChainId.Ethereum,
   });
   expect(privateKey).not.toBeNull();
   expect(privateKey).not.toBe(undefined);
+  expect(privateKey).not.toBe('N/A');
 });
