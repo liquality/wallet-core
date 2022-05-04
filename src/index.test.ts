d086dc38363b5d16d12b997066de1dbe54b2a2be
beemi <beemi.raja@gmail.com>
2022-05-04 20:54:31 +0100

    test: create wallet tests

--- src/index.test.ts
+++ src/index.test.ts
@@ -13,39 +13,6 @@ test('Initial State of wallet setup', async () => {
   expect(wallet.state.injectEthereum).toBe(true);
 });
 
-test('should be able to create wallet and change network', async () => {
-  const wallet = await setupWallet(defaultWalletOptions);
-  await wallet.dispatch.createWallet({
-    key: '0x1234567890123456789012345678901234567890',
-    mnemonic: 'test',
-    imported: false,
-  });
-  expect(wallet.state.wallets.length).toBe(1);
-  expect(wallet.state.wallets[0].id).not.toBe(undefined);
-  expect(wallet.state.wallets[0].name).toEqual('Account 1');
-  expect(wallet.state.wallets[0].mnemonic).toBe('test');
-  expect(wallet.state.wallets[0].at).not.toBe(0);
-  expect(wallet.state.unlockedAt).toBe(0);
-  expect(wallet.state.activeNetwork).toBe('mainnet');
-  expect(wallet.state.wallets[0].imported).toBe(false);
-  expect(wallet.state.activeWalletId).not.toBe(null);
-  const numberOfAccounts = wallet.state.accounts;
-  const walletId = wallet.state.activeWalletId;
-  expect(numberOfAccounts[walletId]).not.toBe(undefined);
-  expect(numberOfAccounts[walletId]).toHaveProperty('mainnet');
-  expect(numberOfAccounts[walletId]).toHaveProperty('testnet');
-  expect(wallet.state.enabledChains[walletId]).toHaveProperty('mainnet');
-  expect(wallet.state.enabledChains[walletId]).toHaveProperty('testnet');
-  expect(wallet.state.enabledChains[walletId]).toHaveProperty('testnet');
-  expect(wallet.state.enabledChains[walletId]).toHaveProperty('mainnet');
-
-  // change network
-  await wallet.dispatch.changeActiveNetwork({
-    network: Network.Testnet,
-  });
-  expect(wallet.state.activeNetwork).toBe('testnet');
-});
-
 test('should be able to create wallet and validate mainnet accounts', async () => {
   const wallet = await setupWallet(defaultWalletOptions);
   await wallet.dispatch.createWallet({
