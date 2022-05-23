4055b057f2413ee4bc3e935f3d1f75a76f2e18a3
Jennie <echenimjennifer@gmail.com>
2022-05-23 11:29:16 +0400

    feat: add test for `getNFTAssets`

---
+++ src/store/actions/getNFTAssets.test.ts
@@ -0,0 +1,30 @@
+import { setupWallet } from '../../index';
+import defaultWalletOptions from '../../walletOptions/defaultOptions';
+import { Network } from '../types';
+
+describe('getNFTAssets tests', () => {
+  it('should getNFT assets', async () => {
+    const wallet = await setupWallet(defaultWalletOptions);
+    await wallet.dispatch.createWallet({
+      key: '0x1234567890123456789012345678901234567890',
+      mnemonic: 'test',
+      imported: true,
+    });
+    await wallet.dispatch.unlockWallet({
+      key: '0x1234567890123456789012345678901234567890',
+    });
+    expect(wallet.state.wallets.length).toBe(1);
+    expect(wallet.state.wallets[0].imported).toBe(true);
+    expect(wallet.state.unlockedAt).not.toBe(0);
+
+    const walletId = wallet.state.activeWalletId;
+    await wallet.dispatch.initializeAnalyticsPreferences({
+      accepted: true,
+    });
+    const assets = await wallet.dispatch.getNFTAssets({
+      network: Network.Testnet,
+      walletId: walletId,
+    });
+    expect(assets).toEqual([]);
+  });
+});
