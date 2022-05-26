6071e3bf2223e09824fdea80b611053552b67644
Krasimir Raykov <raykov@weichain.tech>
2022-05-26 17:15:36 +0300

    feat: chainify@1.0.4

--- src/build.config.ts
+++ src/build.config.ts
@@ -115,7 +115,7 @@ const config: WalletCoreConfig = {
         routerAddress: SovrynTestnetAddresses.swapNetwork,
         routerAddressRBTC: SovrynTestnetAddresses.proxy3,
         rpcURL: 'https://testnet.sovryn.app/rpc',
-      }
+      },
     },
     mainnet: {
       liquality: {
@@ -190,7 +190,7 @@ const config: WalletCoreConfig = {
         icon: 'hop.svg',
         type: SwapProviderType.Hop,
         graphqlBaseURL: 'https://api.thegraph.com/subgraphs/name/hop-protocol',
-      }
+      },
     },
   },
   discordUrl: 'https://discord.gg/Xsqw7PW8wk',
