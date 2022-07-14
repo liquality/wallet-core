dec3fa6a8bd0066e462fb9e9acb9d0f56069acbb
AndonMitev <mitevandon94@gmail.com>
2022-07-14 16:52:17 +0300

    fix: if

--- src/store/actions/updateMessages.ts
+++ src/store/actions/updateMessages.ts
@@ -14,27 +14,27 @@ export const updateMessages = async (
 
   const myAddress = accountData?.addresses[0];
 
-  if (!socket) {
-    console.log('in');
-    socket = io('https://9dee-85-196-181-2.eu.ngrok.io', {
-      reconnectionDelayMax: 10000,
-    });
-  }
-
   const _message = { sender: myAddress, recipient, timestamp, message };
 
-  socket.emit('SEND_MESSAGE', { recipient, sender: myAddress, message, timestamp: _message.timestamp });
-  commit.UPDATE_MESSAGES({ ..._message });
+  if (!socket) {
+    console.log('in');
+    socket = io('https://47e5-85-196-181-2.eu.ngrok.io/', {
+      reconnectionDelayMax: 10000,
+    });
 
-  socket.on('SEND_MESSAGE_ACK', async (payload) => {
-    console.log('SEND_MESSAGE_ACK', payload);
-    if (payload.recipient === myAddress) {
-      console.log('hit');
+    socket.emit('SEND_MESSAGE', { recipient, sender: myAddress, message, timestamp: _message.timestamp });
+    commit.UPDATE_MESSAGES({ ..._message });
 
-      // TODO: get private key
-      commit.UPDATE_MESSAGES({ ...payload, recipient: payload.sender });
-    }
-  });
+    socket.on('SEND_MESSAGE_ACK', async (payload) => {
+      console.log('SEND_MESSAGE_ACK', payload);
+      if (payload.recipient === myAddress) {
+        console.log('hit');
+
+        // TODO: get private key
+        commit.UPDATE_MESSAGES({ ...payload, recipient: payload.sender });
+      }
+    });
+  }
 
   return _message;
 };
