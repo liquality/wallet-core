395848a8deae0db5772e63b31cdc132a78cb6183
koderholic <22542946+koderholic@users.noreply.github.com>
2022-08-10 21:17:01 +0100

    fix: updates

--- src/swaps/thorHop/types.ts
+++ src/swaps/thorHop/types.ts
@@ -1,5 +1,5 @@
 import { Transaction } from '@chainify/types';
-import { AccountId, Asset } from '../../store/types';
+import { Asset } from '../../store/types';
 import { ThorchainSwapHistoryItem, ThorchainSwapProviderConfig, ThorchainSwapQuote } from '../thorchain/ThorchainSwapProvider';
 import { NextSwapActionRequest } from '../types';
 import { Chain, TToken } from '@hop-protocol/sdk';
