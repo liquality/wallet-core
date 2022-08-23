fd46bf45c6423971ed2032c2b8a4e35094b19e53
Krasimir Raykov <raykov@weichain.tech>
2022-08-23 08:25:47 +0300

    fix: hack interface & abstract-class symbiosis

--- packages/cryptoassets/src/chains/BaseChain.ts
+++ packages/cryptoassets/src/chains/BaseChain.ts
@@ -1,43 +1,11 @@
 import { ensure0x } from '@chainify/utils';
 import { getAddress, isAddress } from '@ethersproject/address';
-import { IAsset } from '../interfaces/IAsset';
 import { IChain } from '../interfaces/IChain';
-import { IFees } from '../interfaces/IFees';
-import { IGasLimits } from '../interfaces/IGasLimit';
-import { INetwork } from '../interfaces/INetwork';
-import { ChainId, ExplorerView, FeeMultiplier } from '../types';
 
 export abstract class BaseChain implements IChain {
   constructor(chain: IChain) {
     Object.assign(this, chain);
   }
-
-  public id: ChainId;
-  public name: string;
-
-  public code: string;
-  public color: string;
-
-  public nativeAsset: IAsset;
-
-  public isEVM: boolean;
-  public hasTokens: boolean;
-
-  public averageBlockTime: number;
-  public safeConfirmations: number;
-  public txFailureTimeoutMs: number;
-
-  public network: INetwork;
-  public explorerViews: ExplorerView[];
-  public faucetUrl: string;
-
-  public multicallSupport: boolean;
-
-  public EIP1559: boolean;
-  public feeMultiplier: FeeMultiplier;
-  public gasLimit: IGasLimits;
-  public fees: IFees;
-  public supportCustomFees: boolean;
 
   public isValidAddress(address: string) {
     return isAddress(address);
@@ -54,4 +22,9 @@ export abstract class BaseChain implements IChain {
   public formatTransactionHash(hash: string) {
     return ensure0x(hash).toLowerCase();
   }
+}
+
+// eslint-disable-next-line @typescript-eslint/no-empty-interface
+export interface BaseChain extends IChain {
+  //
 }
