89e593836ab55a4313f715d42af07caea02629f1
tima-t <tito.marchev@gmail.com>
2022-08-23 11:51:15 +0300

    fix: remove Matic from supportedBridge asset as it is not supported from the liquality agent (#112)

--- packages/wallet-core/src/build.config.ts
+++ packages/wallet-core/src/build.config.ts
@@ -110,14 +110,14 @@ const config: WalletCoreConfig = {
         type: SwapProviderType.LiqualityBoostNativeToERC20,
         network: Network.Testnet,
         icon: 'liqualityboost.svg',
-        supportedBridgeAssets: ['RBTC', 'MATIC', 'AVAX', 'LUNA', 'UST'],
+        supportedBridgeAssets: ['RBTC', 'AVAX', 'LUNA', 'UST'],
       },
       [SwapProviderType.LiqualityBoostERC20ToNative]: {
         name: 'Liquality Boost',
         type: SwapProviderType.LiqualityBoostERC20ToNative,
         network: Network.Testnet,
         icon: 'liqualityboost.svg',
-        supportedBridgeAssets: ['RBTC', 'MATIC', 'AVAX', 'LUNA', 'UST'],
+        supportedBridgeAssets: ['RBTC', 'AVAX', 'LUNA', 'UST'],
       },
       [SwapProviderType.UniswapV2]: {
         name: 'Uniswap V2',
@@ -159,14 +159,14 @@ const config: WalletCoreConfig = {
         type: SwapProviderType.LiqualityBoostNativeToERC20,
         network: Network.Mainnet,
         icon: 'liqualityboost.svg',
-        supportedBridgeAssets: ['RBTC', 'MATIC', 'AVAX', 'LUNA', 'UST'],
+        supportedBridgeAssets: ['RBTC', 'AVAX', 'LUNA', 'UST'],
       },
       [SwapProviderType.LiqualityBoostERC20ToNative]: {
         name: 'Liquality Boost',
         type: SwapProviderType.LiqualityBoostERC20ToNative,
         network: Network.Mainnet,
         icon: 'liqualityboost.svg',
-        supportedBridgeAssets: ['RBTC', 'MATIC', 'AVAX', 'LUNA', 'UST'],
+        supportedBridgeAssets: ['RBTC', 'AVAX', 'LUNA', 'UST'],
       },
       [SwapProviderType.UniswapV2]: {
         name: 'Uniswap V2',
@@ -250,7 +250,7 @@ const config: WalletCoreConfig = {
     ChainId.Avalanche,
     ChainId.Solana,
   ],
-  supportedBridgeAssets: ['MATIC', 'RBTC', 'AVAX'],
+  supportedBridgeAssets: ['RBTC', 'AVAX'],
   nameResolvers: {
     uns: {
       resolutionService: 'https://unstoppabledomains.g.alchemy.com/domains/',
