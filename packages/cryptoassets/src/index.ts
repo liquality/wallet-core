68b3ffb51ea04b5aedde492c5c8033f207d0e6ab
Krasimir Raykov <raykov@weichain.tech>
2022-08-24 15:28:43 +0300

    feat: export IChain

--- packages/cryptoassets/src/index.ts
+++ packages/cryptoassets/src/index.ts
@@ -2,5 +2,6 @@ export * from './assets';
 export * from './chains';
 export { dappChains } from './dapps';
 export { IAsset } from './interfaces/IAsset';
+export { IChain } from './interfaces/IChain';
 export { AssetType, AssetTypes, ChainId } from './types';
 export { currencyToUnit, unitToCurrency } from './utils';
