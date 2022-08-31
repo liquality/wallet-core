ce0a2c395ac48484948b37d4b02460b0905d6aad
Slav Keremidchiev <skeremidch@gmail.com>
2022-08-31 15:23:13 +0300

    feat: lint no-console rule

--- packages/wallet-core/src/store/migrations/19_remove_existing_ledger_accounts_test.ts
+++ packages/wallet-core/src/store/migrations/19_remove_existing_ledger_accounts_test.ts
@@ -1,5 +1,4 @@
 import { removeExistingLedgerAccounts } from './19_remove_existing_ledger_accounts';
 import state from './19_remove_existing_ledger_accounts.state.pre.json';
 
-const newState = removeExistingLedgerAccounts.migrate(state);
-console.log('newState', newState);
+const _newState = removeExistingLedgerAccounts.migrate(state);
