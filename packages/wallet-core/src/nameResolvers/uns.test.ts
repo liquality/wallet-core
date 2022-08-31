906bd7d81cb62811b765a9152cd5a8de5749c710
Amrit Jain <amritkumarjain321@gmail.com>
2022-08-31 13:45:41 +0530

    feat: chain to asset

--- packages/wallet-core/src/nameResolvers/uns.test.ts
+++ packages/wallet-core/src/nameResolvers/uns.test.ts
@@ -1,15 +1,20 @@
-import { ChainId } from '@liquality/cryptoassets';
-import { chainToUNSKey, UNSResolver } from './uns';
+// import cryptoassets from 'src/utils/cryptoassets';
+import cryptoassets from '../utils/cryptoassets';
+
+import { getUNSKey, UNSResolver } from './uns';
 
 describe('chainToUNSKey', () => {
   it('returns correct key for bitcoin', () => {
-    expect(chainToUNSKey(ChainId.Bitcoin)).toBe('BTC');
+    const bitcoin = cryptoassets['BTC'];
+    expect(getUNSKey(bitcoin)).toBe('crypto.BTC.address');
   });
   it('returns correct key for polygon', () => {
-    expect(chainToUNSKey(ChainId.Polygon)).toBe('MATIC.version.MATIC');
+    const matic = cryptoassets['MATIC'];
+    expect(getUNSKey(matic)).toBe('crypto.MATIC.version.MATIC.address');
   });
   it('returns ETH for unsupported EVM chain', () => {
-    expect(chainToUNSKey(ChainId.Arbitrum)).toBe('ETH');
+    const eth = cryptoassets['ETH'];
+    expect(getUNSKey(eth)).toBe('crypto.ETH.address');
   });
 });
 
@@ -18,7 +23,8 @@ describe('uns resolver', () => {
 
   const unsResolver = new UNSResolver();
   it('should resolve polygon address', async () => {
-    const address = await unsResolver.lookupDomain('shaista.blockchain', ChainId.Polygon);
+    const matic = cryptoassets['MATIC'];
+    const address = await unsResolver.lookupDomain('shaista.blockchain', matic);
     expect(address).toBe('0xada3bccf641da3b1b1553ed70e7f38c7ad1b3023');
   });
 });
