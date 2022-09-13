1152795e9b1a8c0b19cef362284d5f592fb14bdd
Slav Keremidchiev <skeremidch@gmail.com>
2022-09-13 13:16:27 +0300

    fix: 19th migration

--- packages/wallet-core/src/store/migrations/19_remove_existing_ledger_accounts_test.ts
+++ packages/wallet-core/src/store/migrations/19_remove_existing_ledger_accounts_test.ts
@@ -1,4 +1,4 @@
 import { removeExistingLedgerAccounts } from './19_remove_existing_ledger_accounts';
 import state from './19_remove_existing_ledger_accounts.state.pre.json';
 
-const _newState = removeExistingLedgerAccounts.migrate(state);
+removeExistingLedgerAccounts.migrate(state);
