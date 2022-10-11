16dd46467ac789655ebe15d3dcedc403e81553a4
monokh <mnokhb@gmail.com>
2022-10-11 17:31:05 +0100

    feat: switch eth testnet to goerli

--- packages/cryptoassets/src/chains/testnet/evm/ethereum.ts
+++ packages/cryptoassets/src/chains/testnet/evm/ethereum.ts
@@ -5,20 +5,20 @@ import { transformMainnetToTestnetChain } from '../../../chains/utils';
 export default transformMainnetToTestnetChain(
   ethereum,
   {
-    name: 'ropsten',
+    name: 'goerli',
     coinType: '60',
-    networkId: 3,
-    chainId: 3,
+    networkId: 5,
+    chainId: 5,
     isTestnet: true,
-    rpcUrls: ['https://ropsten.infura.io/v3/a2ad6f8c0e57453ca4918331f16de87d'],
+    rpcUrls: ['https://goerli.infura.io/v3/a2ad6f8c0e57453ca4918331f16de87d'],
   },
   [
     {
-      tx: 'https://ropsten.etherscan.io/tx/{hash}',
-      address: 'https://ropsten.etherscan.io/address/{address}',
-      token: 'https://ropsten.etherscan.io/token/{token}',
+      tx: 'https://goerli.etherscan.io/tx/{hash}',
+      address: 'https://goerli.etherscan.io/address/{address}',
+      token: 'https://goerli.etherscan.io/token/{token}',
     },
   ],
-  'https://faucet.metamask.io/',
+  'https://faucet.paradigm.xyz/',
   NftProviderType.Moralis
 );
