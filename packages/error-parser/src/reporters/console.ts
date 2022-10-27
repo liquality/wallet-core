98b1093e18e14c929a08d62a50e39369e47cc153
koderholic <22542946+koderholic@users.noreply.github.com>
2022-10-27 13:16:54 +0100

    fix: update how error is logged to console

--- packages/error-parser/src/reporters/console.ts
+++ packages/error-parser/src/reporters/console.ts
@@ -2,5 +2,15 @@ import { LiqualityErrorJSON } from '../types/types';
 import { LiqualityError } from '../LiqualityErrors/LiqualityError';
 
 export function reportToConsole(error: LiqualityError | LiqualityErrorJSON) {
-  console.log(error);
+  console.error(prepareErrorForConsole(error));
+}
+
+function prepareErrorForConsole(error: LiqualityError | LiqualityErrorJSON) {
+  return `New Error From Error Parser \n
+          ID: ${error.data.errorId} \n
+          Name: ${error.name} \n
+          Developer Message: ${JSON.stringify(error.devMsg)} \n
+          Raw Error: ${JSON.stringify(error.rawError)} \n
+          Data: ${JSON.stringify(error.data)} \n
+          Stack: ${error.stack} \n`;
 }
