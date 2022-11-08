5095b988f1cbfe1e63a5c11a8790a769ad78932e
Krasimir Raykov <raykov@weichain.tech>
2022-11-08 18:33:23 +0200

    chore: rename to solana-wallet-standard

--- packages/solana-injection/src/initialize.ts
+++
@@ -1,7 +0,0 @@
-import { registerWallet } from './register.js';
-import { LiqualityWallet } from './wallet.js';
-import type { Liquality } from './window.js';
-
-export function initialize(Liquality: Liquality): void {
-    registerWallet(new LiqualityWallet(Liquality));
-}
