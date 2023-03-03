1013fea40b4ac63ad36df9e2fb2bb4c1f4635041
Bradley Suira <bradleysuira@gmail.com>
2023-03-03 12:53:00 -0500

    feat: update enable assets with default assets from build config

--- packages/wallet-core/src/index.ts
+++ packages/wallet-core/src/index.ts
@@ -10,6 +10,28 @@ function setupWallet(options: WalletOptions) {
   if (options.initialState) store.commit.SET_STATE({ newState: options.initialState });
   updateErrorReporterConfig({ callback: (error: LiqualityErrorJSON) => store.dispatch.logError(error) });
 
+  // to update enabled assets with default assets in case that can be updated not need a new migration
+  const { enabledAssets, activeWalletId } = store.state;
+  store.commit.SET_STATE({
+    newState: {
+      ...store.state,
+      enabledAssets: {
+        mainnet: {
+          [activeWalletId]: [
+            ...(enabledAssets['mainnet']?.[activeWalletId] || []),
+            ...buildConfig.defaultAssets.mainnet,
+          ].filter((value, index, array) => array.indexOf(value) === index),
+        },
+        testnet: {
+          [activeWalletId]: [
+            ...(enabledAssets['testnet']?.[activeWalletId] || []),
+            ...buildConfig.defaultAssets.testnet,
+          ].filter((value, index, array) => array.indexOf(value) === index),
+        },
+      },
+    },
+  });
+
   return store;
 }
 
